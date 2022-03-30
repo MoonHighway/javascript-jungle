f96d7f220f2eafd853decc12c1179ddef43e4912
Eve Porcello <eve@moonhighway.com>
2022-03-30 11:50:28 -0700

    Update solution-1.js

--- 02-data-structures/lab/solution/solution-1.js
+++ 02-data-structures/lab/solution/solution-1.js
@@ -2,10 +2,10 @@ const users = require("./ages.json");
 
 for (let i = 0; i < users.length; i++) {
   let ageCheckString =
-    users[i].age > 25
-      ? `${users[i].age - 25} years older than`
-      : users[i].age < 25
-      ? `${25 - users[i].age} years younger than`
+    users[i].age > 26
+      ? `${users[i].age - 26} years older than`
+      : users[i].age < 26
+      ? `${26 - users[i].age} years younger than`
       : `the same age as`;
   console.log(`${users[i].name} is ${ageCheckString} JavaScript`);
 }
